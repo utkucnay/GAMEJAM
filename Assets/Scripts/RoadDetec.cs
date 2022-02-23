ed3355f2329c518be20ad583cf79d9b7f2d57da4
utkucnay <utkumay57@gmail.com>
2022-02-23 23:18:43 +0300

    Karakter sonsuza Gidebiliyor

--- Assets/Scripts/RoadDetec.cs
+++ Assets/Scripts/RoadDetec.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class RoadDetec : MonoBehaviour
 {
-
+    public float Distance;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "RoadTrigger")
         {
-            Destroy(other.gameObject.transform.parent.gameObject);
-
+            
+            other.gameObject.transform.parent.gameObject.transform.position += new Vector3(0,0,2*Distance);
         }
-
     }
-
 }
